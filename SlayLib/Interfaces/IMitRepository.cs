2ea3b5beafd51b10be838e992eff6ab1703550b0
Diana Zayats <marusya3651@gmail.com>
2025-10-09 21:26:50 +0300

    Add repository interfaces and update ApplicationUser

---
+++ SlayLib/Interfaces/IMitRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SlayLib.Models;
+
+namespace SlayLib.Interfaces
+{
+    public interface IMitRepository : IRepository
+    {
+        Task<ApplicationUser> GetUserByEmailAsync(string email);
+    }
+}
---
+++ SlayLib/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SlayLib.Interfaces
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<T>> GetAllAsync<T>() where T : class;
+        Task<IEnumerable<T>> FindAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task UpdateAsync<T>(T entity) where T : class;
+        Task RemoveAsync<T>(T entity) where T : class;
+        Task SaveChangesAsync();
+        Task<bool> ExistsAsync<T>(Expression<Func<T, bool>> predicate) where T : class;
+
+    }
+}
