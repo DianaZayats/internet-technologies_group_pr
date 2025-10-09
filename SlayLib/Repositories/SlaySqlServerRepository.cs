c35f74016bb2389cc02961da6fcc21f4bda3af8c
EvaCherkun <evacherkun1@gmail.com>
2025-10-09 22:30:05 +0300

    Add SlaySqlServerRepository with GetByEmailAsync method

---
+++ SlayLib/Repositories/SlaySqlServerRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SlayLib.Models;
+namespace SlayLib.Repositories
+{
+    public class SlaySqlServerRepository<TDbContext> : BaseSqlServerRepository<TDbContext>
+        where TDbContext : DbContext
+    {
+        public SlaySqlServerRepository(TDbContext db) : base(db)
+        {
+        }
+
+       
+        public async Task<ApplicationUser?> GetByEmailAsync(string email)
+        {
+            var users = await FindAsync<ApplicationUser>(u => u.Email == email);
+            return users.FirstOrDefault();
+        }
+    }
+}
